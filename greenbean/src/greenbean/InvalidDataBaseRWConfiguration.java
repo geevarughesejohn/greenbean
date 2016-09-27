0863d66321d079ef9cbda1602e081f1733ca9da6
geevarughesejohn <geevarughese84@gmail.com>
2016-09-27 10:35:05 +0530

    Configuration

---
+++ greenbean/src/greenbean/InvalidDataBaseRWConfiguration.java
@@ -0,0 +1,11 @@
+package greenbean;
+
+public class InvalidDataBaseRWConfiguration extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public InvalidDataBaseRWConfiguration(String message) {
+		super(message);
+	}
+
+}
